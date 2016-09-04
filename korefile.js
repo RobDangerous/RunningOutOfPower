f9b5f7e20ba5f66865ca78aaf780cf6e6b703e60
Robert Konrad <git2016@robdangero.us>
2016-09-04 23:16:49 +0200

    Update Kore and korefile

--- korefile.js
+++ korefile.js
@@ -1,11 +1,8 @@
-var solution = new Solution('BlocksFromHeaven');
 var project = new Project('BlocksFromHeaven');
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
 
-project.addSubProject(Solution.createProject('Kore'));
+project.addSubProject(Project.createProject('Kore'));
 
-solution.addProject(project)
-
-return solution;
+return project;
